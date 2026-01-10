b4ec64670276bdfbebf112ed274a80a3adddbe94
trinhxuangiang16 <trinhgiang.dev16@gmail.com>
2026-01-11 02:54:29 +0700

    update

--- src/layouts/HomeLayout/HomeLayout.jsx
+++ src/layouts/HomeLayout/HomeLayout.jsx
@@ -1,8 +1,9 @@
 import { Outlet } from "react-router-dom";
-
+import { NavLink } from "react-router-dom";
 export default function HomeLayout() {
   return (
     <div className="home-layout">
+      <NavLink to="/admin">Admin</NavLink>
       Home Layout
       <Outlet />
     </div>
