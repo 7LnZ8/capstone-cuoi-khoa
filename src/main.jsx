aa580c46f8b50db7bf973093aa71facdeb97d18a
trinhxuangiang16 <trinhgiang.dev16@gmail.com>
2026-01-11 02:29:59 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,17 @@
 // src/main.jsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import { Provider } from 'react-redux';
-import { store } from './app/store.js'; // Import store vừa tạo ở bước 1
-import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'; // Import React Query
-import './styles/style.scss'; // Import file css chính (nếu có)
-// Hoặc import 'antd/dist/reset.css'; nếu dùng antd v4, v5 tự động không cần
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import { store } from "./app/store.js"; // Import store vừa tạo ở bước 1
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; // Import React Query
+import "antd/dist/reset.css";
 
 // Tạo client cho React Query
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
@@ -21,5 +20,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         </BrowserRouter>
       </QueryClientProvider>
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
